e7754fa0f7758511fc195c2121da883dca1cbd1e
cal101 <cal101@users.noreply.github.com>
2017-05-20 00:16:09 +0200

    global contract related definitions

---
+++ src/main/java/org/walkmod/javalang/Contracts.java
@@ -0,0 +1,13 @@
+package org.walkmod.javalang;
+
+/**
+ * Global contract related definitions.
+ */
+public final class Contracts {
+    /** invariant checking maybe expensive so it's not enabled per default */
+    public static final boolean CHECK_INVARIANT_ENABLED = false;
+
+    /** invariants that are not ready for general use  */
+    public static final boolean CHECK_EXPERIMENTAL_INVARIANT_ENABLED = false;
+}
+                                       
